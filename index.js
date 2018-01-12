221431fe2aed480aeacca56e9b2d9d81e3972b47
tunde Oretade <tundeiness@yahoo.co.uk>
2018-01-12 22:44:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function nowServing(katzDeliLine){
 }
 
 
+function currentLine(katzDeliLine){
+  
+    if(katzDeliLine.length > 0){
+        return `The line is currently: ${katzDeliLine}`;
+    }else{
+        return `The line is currently empty.`;
+    }
+}
