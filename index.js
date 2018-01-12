d1b9f5e12b50a6c21c7ea22d1b6352b2262ae346
tunde Oretade <tundeiness@yahoo.co.uk>
2018-01-12 22:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, customer){
-          katzDeliLine.push();
-          return `Welcome, ${customer}. You are number ${katzDeliLine.length + 1} in line.`;
+          katzDeliLine.push(customer);
+          return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`;
 }
