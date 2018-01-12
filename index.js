5683f9ad9e4a800f996eddd5094420e35895daa5
tunde Oretade <tundeiness@yahoo.co.uk>
2018-01-12 22:43:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ function takeANumber(katzDeliLine, customer){
           katzDeliLine.push(customer);
           return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`;
 }
+
+
+
+
+
+function nowServing(katzDeliLine){
+  
+     if(katzDeliLine.length > 0){
+    	return `Currently serving ${katzDeliLine.shift()}.`;
+    } else {
+    	return `There is nobody waiting to be served!`;
+      }
+}
+
+
