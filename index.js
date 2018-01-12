b26e00c6b6ad39ddf32ae2b36c06a544bd7ee0de
tunde Oretade <tundeiness@yahoo.co.uk>
2018-01-12 22:50:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(katzDeliLine){
   
     if(katzDeliLine.length > 0){
           for(var i = 0; i < katzDeliLine.length; i++){
-        return `The line is currently: ${katzDeliLine}`;
+        return `The line is currently: ${i+1} ${katzDeliLine}`;
           }
     }else{
         return `The line is currently empty.`;
